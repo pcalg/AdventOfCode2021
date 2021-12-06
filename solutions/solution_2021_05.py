a38dc793b7b9f692e04efcd338d0d87ef14ebd54
pcalg <pcalgra@hotmail.com>
2021-12-06 08:17:02 +0100

    day 5 - solution 1 (updated) and 2

--- solutions/solution_2021_05.py
+++ solutions/solution_2021_05.py
@@ -8,23 +8,27 @@ test = False
 
 puzzle_input = read_day(5, test)
 
-# [pos.split(",") for pos in s.split(" -> ")]
+
+def sign(value):
+    if value == 0:
+        return 0
+    if value < 0:
+        return -1
+    else:
+        return 1
+
+
 def parse_input(puzzle_input):
-    #lines = {}
     lines = []
 
     for line in puzzle_input:
         line_parts = [pos.split(",") for pos in line.split(" -> ")]
-        #start_pos, end_pos = line_parts
         lines.append(line_parts)
-
-
-        #lines[tuple(int(coord) for coord in start_pos)] = tuple(int(coord) for coord in end_pos)
 
     return lines
 
-class PuzzleDay5(PuzzleInterface):
 
+class PuzzleDay5(PuzzleInterface):
 
     def solve_part_1(self):
         lines = parse_input(self.puzzle_contents)
@@ -32,34 +36,52 @@ class PuzzleDay5(PuzzleInterface):
         straight_lines = [line for line in lines if line[0][0] == line[1][0] or line[0][1] == line[1][1]]
 
         diagram = defaultdict(int)
-        for line in straight_lines:
-            x_vals = sorted([int(line[0][0]), int(line[1][0])])
-            y_vals = sorted([int(line[0][1]), int(line[1][1])])
 
-            for x in range(x_vals[0], x_vals[1] + 1):
-                for y in range(y_vals[0], y_vals[1] + 1 ):
-                    diagram[(x, y)] += 1
+        for line in straight_lines:
+            x1, y1 = int(line[0][0]), int(line[0][1])
+            x2, y2 = int(line[1][0]), int(line[1][1])
+
+            if x1 == x2:
+                distance = abs(y2 - y1)
+                dx = 0
+                dy = sign(y2 - y1)
+            else:  # y1 == y2:
+                distance = abs(x2 - x1)
+                dx = sign(x2 - x1)
+                dy = 0
+
+            for n in range(distance + 1):
+                diagram[(x1 + n * dx, y1 + n * dy)] += 1
 
         # now check for values > 0 (overlap)
         return len([c for c in diagram.values() if c > 1])
-
 
     def solve_part_2(self):
         lines = parse_input(self.puzzle_contents)
-        # only straight lines
-
         diagram = defaultdict(int)
+
         for line in lines:
-            x_vals = sorted([int(line[0][0]), int(line[1][0])])
-            y_vals = sorted([int(line[0][1]), int(line[1][1])])
+            x1, y1 = int(line[0][0]), int(line[0][1])
+            x2, y2 = int(line[1][0]), int(line[1][1])
 
-            for x in range(x_vals[0], x_vals[1] + 1):
-                for y in range(y_vals[0], y_vals[1] + 1 ):
-                    diagram[(x, y)] += 1
+            if x1 == x2:
+                distance = abs(y2 - y1)
+                dx = 0
+                dy = sign(y2 - y1)
+            elif y1 == y2:
+                distance = abs(x2 - x1)
+                dx = sign(x2 - x1)
+                dy = 0
+            else:
+                # 45 degrees, so only consider one (either x or y) for distance
+                distance = abs(y2 - y1)
+                dx = sign(x2 - x1)
+                dy = sign(y2 - y1)
 
-        # now check for values > 0 (overlap)
+            for n in range(distance + 1):
+                diagram[(x1 + n * dx, y1 + n * dy)] += 1
+
         return len([c for c in diagram.values() if c > 1])
-
 
 
 puzzle = PuzzleDay5(puzzle_input)
