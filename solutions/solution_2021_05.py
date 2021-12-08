b2169cf8834656004a49212d68b59b3a20c44ad6
pcalg <pcalgra@hotmail.com>
2021-12-08 21:50:01 +0100

    day 6

--- solutions/solution_2021_05.py
+++ solutions/solution_2021_05.py
@@ -27,31 +27,36 @@ def parse_input(puzzle_input):
 
     return lines
 
+def process_line(diagram, line):
+    x1, y1 = int(line[0][0]), int(line[0][1])
+    x2, y2 = int(line[1][0]), int(line[1][1])
+
+    if y1 == y2:
+        distance = abs(x2 - x1)
+    else:
+        # either x1 == x2 or 45 degrees, so only consider one (either x or y) for distance
+        distance = abs(y2 - y1)
+
+    dx = sign(x2 - x1)
+    dy = sign(y2 - y1)
+
+    for n in range(distance + 1):
+        diagram[(x1 + n * dx, y1 + n * dy)] += 1
+
+    return diagram
 
 class PuzzleDay5(PuzzleInterface):
 
     def solve_part_1(self):
         lines = parse_input(self.puzzle_contents)
+
         # only straight lines
         straight_lines = [line for line in lines if line[0][0] == line[1][0] or line[0][1] == line[1][1]]
 
         diagram = defaultdict(int)
 
         for line in straight_lines:
-            x1, y1 = int(line[0][0]), int(line[0][1])
-            x2, y2 = int(line[1][0]), int(line[1][1])
-
-            if x1 == x2:
-                distance = abs(y2 - y1)
-                dx = 0
-                dy = sign(y2 - y1)
-            else:  # y1 == y2:
-                distance = abs(x2 - x1)
-                dx = sign(x2 - x1)
-                dy = 0
-
-            for n in range(distance + 1):
-                diagram[(x1 + n * dx, y1 + n * dy)] += 1
+            diagram = process_line(diagram, line)
 
         # now check for values > 0 (overlap)
         return len([c for c in diagram.values() if c > 1])
@@ -61,26 +66,9 @@ class PuzzleDay5(PuzzleInterface):
         diagram = defaultdict(int)
 
         for line in lines:
-            x1, y1 = int(line[0][0]), int(line[0][1])
-            x2, y2 = int(line[1][0]), int(line[1][1])
+            diagram = process_line(diagram, line)
 
-            if x1 == x2:
-                distance = abs(y2 - y1)
-                dx = 0
-                dy = sign(y2 - y1)
-            elif y1 == y2:
-                distance = abs(x2 - x1)
-                dx = sign(x2 - x1)
-                dy = 0
-            else:
-                # 45 degrees, so only consider one (either x or y) for distance
-                distance = abs(y2 - y1)
-                dx = sign(x2 - x1)
-                dy = sign(y2 - y1)
-
-            for n in range(distance + 1):
-                diagram[(x1 + n * dx, y1 + n * dy)] += 1
-
+        # now check for values > 0 (overlap)
         return len([c for c in diagram.values() if c > 1])
 
 
