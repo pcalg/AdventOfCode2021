928ddedfb64225e21dfd97c04f22555386d2e050
pcalg <pcalgra@hotmail.com>
2021-12-20 09:52:08 +0100

    day 15 - intermediate

--- solutions/solution_2021_15.py
+++ solutions/solution_2021_15.py
@@ -1,9 +1,36 @@
 from general.general import read_day
 from general.puzzle import PuzzleInterface
+import heapq
 
 test = True
 
 puzzle_input = read_day(15, test)
+
+
+def create_grid(puzzle_input):
+    grid = {}
+
+    for y, line in enumerate(puzzle_input):
+        for x, ch in enumerate(line):
+            grid[(y, x)] = int(ch)
+
+    return grid
+
+
+def goal(grid):
+    max_y = max([y for y, _ in grid.keys()])
+    max_x = max([x for _, x in grid.keys()])
+    return max_y, max_x
+
+
+def neighbours(grid, visited, position):
+    y, x = position
+
+    for dy in range(-1, 2):
+        for dx in range(-1, 2):
+            neighbour = y + dy, x + dx
+            if neighbour != position and neighbour in grid and neighbour not in visited:
+                yield neighbour
 
 
 class PuzzleDay15(PuzzleInterface):
@@ -16,6 +43,16 @@ class PuzzleDay15(PuzzleInterface):
 
 
 puzzle = PuzzleDay15(puzzle_input)
+
+g = create_grid(puzzle_input)
+
+y_goal, x_goal = goal(g)
+
+# find shortest route
+h = []
+heapq.heappush(h, (0, (0, 0)))
+visited = set()
+
 
 print(f"Solution: {puzzle.solve_part_1()}")
 print(f"Solution: {puzzle.solve_part_2()}")
